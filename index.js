739e84736f0bfb59d74dee418128de397185992e
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-05-22 13:08:55 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NzEzNDI0NDQ3NTI0MzA2OTQ0.Xsf8ig.1LM1WOrHMNlWRlHl5_ACbkBSsYw');

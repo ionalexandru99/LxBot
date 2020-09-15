9b505aa9fe47f9d5c4870b88fbfc5061fdeeb954
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-09-14 23:59:46 -0400

    Complete Switch games menu functions

--- commands/modules/switch.js
+++ commands/modules/switch.js
@@ -1,54 +1,176 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const db = require('./dbInterface');
-const { switchIcon, eshopURL, eshopImageURL } = require('../../config.json');
-
+const { switchIcon } = require('../../config.json');
 
 module.exports = {
 
 	async check(message) {
 
-		const storeUrl = 'https://www.nintendo.com/games/detail/sword-art-online-fatal-bullet-complete-edition-switch/';
-		const page = await fetch(storeUrl).then(response => response.text());
+		const messages = [];
 
-		// Grab game ID
-		const gameIdStart = page.search('platform-label nsuid="');
-		let gameId = page.substring(gameIdStart + 22);
-		const gameIdEnd = gameId.search('"');
-		gameId = gameId.substring(0, gameIdEnd);
+		const topMenuEmbed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setDescription('Choose an action to take by typing a number to execute your requested action. Type **exit** to cancel.'
+				+ '\n'
+				+ '\n 1) View tracked games'
+				+ '\n 2) Add a game'
+				+ '\n 3) Remove a game')
+			.setAuthor('Nintendo Switch Deals', switchIcon);
 
-		// Grab game title
-		const gameTitleStart = page.search('game-title">');
-		let gameTitle = page.substring(gameTitleStart + 12);
-		const gameTitleEnd = gameTitle.search('</h1>');
-		gameTitle = gameTitle.substring(0, gameTitleEnd);
+		function viewGamesEmbed(list) {
+			if (list.length > 0) {
+				return (
+					new Discord.MessageEmbed(topMenuEmbed)
+						.setDescription('Here is the list of currently tracked games:'
+							+ '\n\n'
+							+ list.map(game => '- ' + game).sort().join('\n'))
+				);
+			} else {
+				return (
+					new Discord.MessageEmbed(topMenuEmbed)
+						.setDescription('There are currently no tracked games.')
+				);
+			}
+		}
 
-		// Grab game image
-		const slug = storeUrl.substring(storeUrl.search('detail/') + 7);
-		const imageURL = eshopImageURL + slug + slug.substring(0, slug.length - 1) + '-hero.jpg';
+		const editGamesEmbed = new Discord.MessageEmbed(topMenuEmbed)
+			.setDescription('Enter the URL for the game as found on the Nintendo eShop'
+				+ '\n\n Example: `https://www.nintendo.com/games/detail/animal-crossing-new-horizons-switch/`');
 
-		// Grab prices and calculate discount
-		const { prices } = await fetch(eshopURL + gameId).then(response => response.json());
+		function editGamesSuccessEmbed(name, action) {
+			if (action === 'add')
+				return new Discord.MessageEmbed(topMenuEmbed).setDescription(name + ' successfully added!');
+			else if (action === 'delete')
+				return new Discord.MessageEmbed(topMenuEmbed).setDescription(name + ' successfully removed!');
+		}
 
-		const discountEndDate = new Date(prices[0].discount_price.end_datetime);
-		const regularPrice = Number.parseFloat(prices[0].regular_price.amount.substring(1));
-		const discountPrice = Number.parseFloat(prices[0].discount_price.amount.substring(1));
-		const discount = ((regularPrice - discountPrice) / regularPrice) * 100;
+		function editGamesFailureEmbed(action) {
+			if (action === 'remove') {
+				return new Discord.MessageEmbed(topMenuEmbed).setDescription('That game is not on the list!');
+			} else if (action === 'add') {
+				return new Discord.MessageEmbed(topMenuEmbed).setDescription('That game is already on the list!');
+			}
+		}
 
-		const messageEmbed = new Discord.MessageEmbed()
-			.setColor('#E60012')
-			.setTitle(gameTitle)
-			.setURL(storeUrl)
-			.setAuthor('Nintendo Switch Deals', switchIcon)
-			.setImage(imageURL)
-			.addField('Regular Price', '~~$' + regularPrice + '~~', true)
-			.addField('Sale Price', '$' + discountPrice, true)
-			.addField('Discount', discount.toPrecision(2) + '%', true)
-			// .setDescription('**Select an option by entering its number:**' +
-			// 	'\n1) View List' +
-			// 	'\n2) Add to List' +
-			// 	'\n3) Remove from List');
-			.setFooter('Sale ends ' + discountEndDate.toLocaleString('en-US', { timezone: 'America/New_York' }));
-		message.channel.send(messageEmbed);
+		async function getGameTitle(url) {
+
+			const page = await fetch(url).then(response => response.text());
+
+			// Grab game ID
+			const gameIdStart = page.search('platform-label nsuid="');
+			let gameId = page.substring(gameIdStart + 22);
+			const gameIdEnd = gameId.search('"');
+			gameId = gameId.substring(0, gameIdEnd);
+
+			// Grab game title
+			const gameTitleStart = page.search('game-title">');
+			let gameTitle = page.substring(gameTitleStart + 12);
+			const gameTitleEnd = gameTitle.search('</h1>');
+			return gameTitle.substring(0, gameTitleEnd);
+		}
+
+		function deleteMessages() {
+			messages.forEach(obj => obj.delete().catch(console.error));
+		}
+		const mainFilter = response => {
+			return response.content === '1' || response.content === '2' || response.content === '3' || response.content == 'exit';
+		};
+		const editFilter = response => {
+			return response.content.includes('https://www.nintendo.com/games/detail/') || response.content == 'exit';
+		};
+
+		message.channel.send(topMenuEmbed).then((msg) => {
+			messages.push(msg);
+			message.channel.awaitMessages(mainFilter, { max: 1, time: 15000, errors: ['time'] })
+				.then(collected => {
+					switch (`${collected.first()}`) {
+						// View games
+						case '1':
+							db.listSwitch().then(list => {
+								messages.push(message.channel.lastMessage);
+								deleteMessages();
+								message.channel.send(viewGamesEmbed(list));
+							});
+							break;
+						// Add games
+						case '2':
+							messages.push(message.channel.lastMessage);
+							message.channel.send(editGamesEmbed).then((msg) => {
+								messages.push(msg);
+								message.channel.awaitMessages(editFilter, { max: 1, time: 15000, errors: ['time'] })
+									.then(collected => {
+										switch (`${collected.first()}`) {
+											case 'exit':
+												messages.push(message.channel.lastMessage);
+												deleteMessages();
+												message.channel.send('Menu closed.');
+												break;
+											default:
+												getGameTitle(`${collected.first()}`).then(game => {
+													db.addSwitch(game, `${collected.first()}`).then(name => {
+														messages.push(message.channel.lastMessage);
+														deleteMessages();
+														if (name !== '') {
+															message.channel.send(editGamesSuccessEmbed(name, 'add'));
+														} else {
+															message.channel.send(editGamesFailureEmbed('add'));
+														}
+													});
+												}
+												);
+										}
+									})
+									.catch(collected => {
+										deleteMessages();
+										message.channel.send('Menu has been closed due to inactivity.');
+									})
+							});
+							break;
+						// Remove games
+						case '3':
+							messages.push(message.channel.lastMessage);
+							message.channel.send(editGamesEmbed).then((msg) => {
+								messages.push(msg);
+								message.channel.awaitMessages(editFilter, { max: 1, time: 15000, errors: ['time'] })
+									.then(collected => {
+										switch (`${collected.first()}`) {
+											case 'exit':
+												messages.push(message.channel.lastMessage);
+												deleteMessages();
+												message.channel.send('Menu closed.');
+												break;
+											default:
+												db.deleteSwitch(`${collected.first()}`).then((game) => {
+													messages.push(message.channel.lastMessage);
+													deleteMessages();
+													if (game !== '') {
+														message.channel.send(editGamesSuccessEmbed(game, 'delete'));
+													} else {
+														message.channel.send(editGamesFailureEmbed('remove'));
+													}
+												});
+										}
+									})
+									.catch(collected => {
+										deleteMessages();
+										message.channel.send('Menu has been closed due to inactivity.');
+									})
+							});
+							break;
+						case 'exit':
+							messages.push(message.channel.lastMessage);
+							deleteMessages();
+							message.channel.send('Menu closed.');
+							break;
+						default:
+							console.error;
+					}
+				})
+				.catch(collected => {
+					deleteMessages();
+					message.channel.send('Menu has been closed due to inactivity.');
+				});
+		});
 	},
 };
--- commands/modules/update.js
+++ commands/modules/update.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const db = require('./dbInterface');
 const fetch = require('node-fetch');
-const { psIcon, psStoreURL } = require('../../config.json');
+const { psIcon, psStoreURL, switchIcon, eshopURL, eshopImageURL } = require('../../config.json');
 
 module.exports = {
 
@@ -49,43 +49,60 @@ module.exports = {
                                 db.updateOnSale(platform, game.url);
                             }
                         }
-                    // case 'switch':
-                    //     const { included } = await fetch(psStoreURL + game.url.substring(43)).then(response => response.json());
-                    //     const contentInfo = included[0].attributes;
-                    //     const contentPrice = contentInfo.skus[0].prices;
+                    case 'switch':
+                        const page = await fetch(url).then(response => response.text());
 
-                    //     if (contentPrice['plus-user']['discount-percentage'] !== 0 || contentPrice['non-plus-user']['discount-percentage'] !== 0) {
-                    //         // Discount available
-                    //         if (game.onSale === false) {
-                    //             let discount;
-                    //             if (contentPrice['plus-user']['discount-percentage'] > contentPrice['non-plus-user']['discount-percentage']) {
-                    //                 discount = contentPrice['plus-user'];
-                    //             }
-                    //             else {
-                    //                 discount = contentPrice['non-plus-user'];
-                    //             }
-                    //             const discountEndDate = new Date(discount.availability['end-date']);
+                        // Grab game ID
+                        const gameIdStart = page.search('platform-label nsuid="');
+                        let gameId = page.substring(gameIdStart + 22);
+                        const gameIdEnd = gameId.search('"');
+                        gameId = gameId.substring(0, gameIdEnd);
+                        console.log(gameID);
 
-                    //             const messageEmbed = new Discord.MessageEmbed()
-                    //                 .setColor('#0099ff')
-                    //                 .setTitle(contentInfo.name)
-                    //                 .setURL(game.url)
-                    //                 .setAuthor('PlayStation 4 Deals', psIcon)
-                    //                 .setImage(contentInfo['thumbnail-url-base'])
-                    //                 .addField('Regular Price', '~~' + discount['strikethrough-price']['display'] + '~~', true)
-                    //                 .addField('Sale Price', discount['actual-price']['display'], true)
-                    //                 .addField('Discount', discount['discount-percentage'] + '%', true)
-                    //                 .setFooter('Sale ends ' + discountEndDate.toLocaleString('en-US', { timezone: 'America/New_York' }));
-                    //             channel.send(messageEmbed);
-                    //             db.updateOnSale(game.url);
-                    //         }
+                        // Grab game title
+                        const gameTitleStart = page.search('game-title">');
+                        let gameTitle = page.substring(gameTitleStart + 12);
+                        const gameTitleEnd = gameTitle.search('</h1>');
+                        gameTitle = gameTitle.substring(0, gameTitleEnd);
+                        console.log(gameTitle);
 
-                    //     } else {
-                    //         // No discount
-                    //         if (game.onSale === true) {
-                    //             db.updateOnSale(game.url);
-                    //         }
-                    //     }
+                        // Grab game image
+                        const slug = storeUrl.substring(storeUrl.search('detail/') + 7);
+                        const imageURL = eshopImageURL + slug + slug.substring(0, slug.length - 1) + '-hero.jpg';
+                        console.log(imageURL);
+
+                        // Grab prices and calculate discount
+                        const { prices } = await fetch(eshopURL + gameId).then(response => response.json());
+
+                        const discountEndDate = new Date(prices[0].discount_price.end_datetime);
+                        const regularPrice = Number.parseFloat(prices[0].regular_price.amount.substring(1));
+                        const discountPrice = Number.parseFloat(prices[0].discount_price.amount.substring(1));
+                        const discount = ((regularPrice - discountPrice) / regularPrice) * 100;
+                        console.log(discount);
+
+                        if (discount !== 0) {
+                            // Discount available
+                            if (game.onSale === false) {
+                                const messageEmbed = new Discord.MessageEmbed()
+                                    .setColor('#E60012')
+                                    .setTitle(gameTitle)
+                                    .setURL(game.url)
+                                    .setAuthor('Nintendo Switch Deals', switchIcon)
+                                    .setImage(imageURL)
+                                    .addField('Regular Price', '~~$' + regularPrice + '~~', true)
+                                    .addField('Sale Price', '$' + discountPrice, true)
+                                    .addField('Discount', discount.toPrecision(2) + '%', true)
+                                    .setFooter('Sale ends ' + discountEndDate.toLocaleString('en-US', { timezone: 'America/New_York' }));
+                                channel.send(messageEmbed);
+                                db.updateOnSale(game.url);
+                            }
+
+                        } else {
+                            // No discount
+                            if (game.onSale === true) {
+                                db.updateOnSale(game.url);
+                            }
+                        }
                 }
             });
         });

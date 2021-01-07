d25b7defa9db4c57adf9ab5baac7e52db86c90cc
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2021-01-06 22:54:50 -0500

    Update deals.js
    Fix issue with deactivating and activating channels

--- commands/deals.js
+++ commands/deals.js
@@ -40,7 +40,7 @@ module.exports = {
 
 		// Embed for channel activation
 		function activateChannel(message) {
-			db.addChannel(message.channel).then(savedChannel => {
+			db.addChannel(message.channel).then(async (savedChannel) => {
 
 				configEmbed.setTitle('Channel Management');
 				if (savedChannel === 'none') {
@@ -57,7 +57,7 @@ module.exports = {
 		//Embed for channel deactivation
 		function deactivateChannel(message) {
 			db.deleteChannel().then(
-				channel => {
+				async (channel) => {
 					configEmbed.setTitle('Channel Deactivation')
 					if (channel) {
 						configEmbed.setDescription('**#' + channel + '** is closed!');

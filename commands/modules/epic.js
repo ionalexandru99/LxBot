9f8e23eb922f3d2facf20421d9709b8a385da40e
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2021-12-13 14:46:25 -0500

    Update epic.js
    - Adjust logic for game editions or DLC
    - Adjust logic for obtaining developer/publisher

--- commands/modules/epic.js
+++ commands/modules/epic.js
@@ -10,7 +10,6 @@ module.exports = {
 		const { data } = await fetch(process.env.epicFreeURL).then(response => response.json());
 
 		// Remove games not yet available for free
-		const currentDate = new Date();
 		const freeEpicGames = data.Catalog.searchStore.elements.filter(game => {
 			if (game.promotions)
 				return (game.promotions.promotionalOffers.length > 0) ? true : false;
@@ -18,7 +17,6 @@ module.exports = {
 
 		freeEpicGames.map(async game => {
 
-			console.log(game.keyImages[0].url);
 			// Grab game information
 			let gameURL = process.env.epicStoreURL;
 			if (game.productSlug.substring(game.productSlug.length - 5) === "/home")
@@ -26,19 +24,42 @@ module.exports = {
 			else
 				gameURL += game.productSlug;
 
+			//Parse productSlug for game editions/DLC
+			if (gameURL.includes('--')) {
+				const urlItems = gameURL.split('--');
+				gameURL = urlItems[0];
+			}
+
+			//Obtain selective content from game JSON
 			const { pages } = await fetch(gameURL).then(response => response.json());
 			const gamePage = pages.find(content => {
 				return content._title === 'home';
 			});
 
+			//Assign developer and publisher when possible
+			let developer = gamePage.data.meta.developer ? gamePage.data.meta.developer : '-';
+			let publisher = gamePage.data.meta.publisher ? gamePage.data.meta.publisher : '-';
+
+			for (attr = 0; attr < game.customAttributes.length; attr++) {
+				if (game.customAttributes[attr].key == 'developerName')
+					developer = game.customAttributes[attr].value;
+				if (game.customAttributes[attr].key == 'publisherName')
+					publisher = game.customAttributes[attr].value;
+			}
+
 			// Convert category tags to title case
-			const tags = gamePage.data.meta.tags.map(tag => {
-				tag = tag.toLowerCase();
-				const words = tag.split('_').map(word => {
-					return word.substring(0, 1).toUpperCase() + word.substring(1);
+			let tags = ['N/A'];
+			try {
+				tags = gamePage.data.meta.tags.map(tag => {
+					tag = tag.toLowerCase();
+					const words = tag.split('_').map(word => {
+						return word.substring(0, 1).toUpperCase() + word.substring(1);
+					});
+					return words.join(' ');
 				});
-				return words.join(' ');
-			});
+			} catch (error) {
+				console.log('No tags found for ' + game.title);
+			}
 
 			// Determine the end date for the promotion
 			const endDate = new Date(game.promotions.promotionalOffers[0].promotionalOffers[0].endDate);
@@ -53,8 +74,8 @@ module.exports = {
 				.setDescription(
 					gamePage.data.about.shortDescription ? gamePage.data.about.shortDescription : '')
 				.setImage(encodeURI(game.keyImages[0].url))
-				.addField('Developer', gamePage.data.meta.developer, true)
-				.addField('Publisher', gamePage.data.meta.publisher, true)
+				.addField('Developer', developer, true)
+				.addField('Publisher', publisher, true)
 				.addField('Tags', tags.join(', '), true)
 				.setFooter('Free until ' + endDate.toLocaleString('en-US', { timeZone: 'America/New_York' }));
 
